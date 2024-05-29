ba6014f74a66e6b0a2535d86ba1490ed93bd0606
DevThanatos74 <calderonalejandrodev@gmail.com>
2024-05-29 17:29:22 -0500

    Commit 3

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ async function main() {
   try {
     await connectDB();
     app.listen(PORT);
+    console.log(`Listening on port http://localhost:${PORT}`);
+    console.log(`Environment: ${process.env.NODE_ENV}`)
   } catch (error) {
     console.error(error);
   }
