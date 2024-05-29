5bc46c14e813b4eddca78fb40dd007493820c499
DevThanatos74 <calderonalejandrodev@gmail.com>
2024-05-29 17:19:10 -0500

    Commit

--- src/config.js
+++ src/config.js
@@ -3,4 +3,4 @@ export const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb+srv://mongo:amaterasu74@cluster0.ve7mixd.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0";
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
 
-export const FRONTEND_URL = process.env.FRONTEND_URL || "https://reservas-vuelos-front.vercel.app/";
+export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
