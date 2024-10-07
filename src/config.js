64ac420c05e91f866f09c8fa55e75f2372c06e9e
AlejandroDev74 <calderonalejandrodev@gmail.com>
2024-10-07 16:57:09 -0500

    Commit cambio de contraseña mongo db

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const PORT = process.env.PORT || 4000;
 export const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb+srv://mongo:amaterasu74@cluster0.ve7mixd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  process.env.MONGODB_URI || "mongodb+srv://mongo:amaterasu54321@cluster0.ve7mixd.mongodb.net/";
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
 
 export const FRONTEND_URL = process.env.FRONTEND_URL || "https://reservas-vuelos-front.vercel.app";
